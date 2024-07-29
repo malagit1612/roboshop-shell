232a03aaab4b4773165050efe7b6c4c199eb84ff
Malathi <krishnavalluru99@gmail.com>
2024-07-29 09:23:26 +0530

    added 2nd script

--- mongodb.sh
+++ mongodb.sh
@@ -32,7 +32,7 @@ cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
 
 VALIDATE $? "Copied MongoDB Repo" 
 
-dnf install mongodb-org -y 
+dnf install mongodb-org -y &>> $LOGFILE
 
 VALIDATE $? "Installing MongoDB"
 
